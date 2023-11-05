cd2250ab0d0edf5af4105aa9df9d2a2f1d924278
aleksejvalenkov <aleksejvalenkov@yandex.ru>
2023-11-05 12:54:39 +0300

    add test cam 2

--- test_camera.py
+++ test_camera.py
@@ -1,8 +1,25 @@
-from picamera import PiCamera
+# Подключаем необходимые библиотеки
+from picamera2 import Picamera2, Preview
 from time import sleep
 
-camera = PiCamera()
+# Создаём объект для работы с камерой
+camera = Picamera2()
 
-camera.start_preview()
+# Настраиваем окно предпросмотра
+preview_config = camera.create_preview_configuration()
+camera.configure(preview_config)
+
+# Запускаем окно предпросмотра сигнала с камеры на экране поверх всех окон
+camera.start_preview(Preview.QTGL)
+
+# Включаем камеру
+camera.start()
+
+# Ставим паузу на 10 секунд
 sleep(10)
+
+# Выключаем камеру
+camera.stop()
+
+# Выключаем окно предпросмотра
 camera.stop_preview()
