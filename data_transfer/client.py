84efeabe3bdc910b681a1d9ea4298d66045f3d80
aleksejvalenkov <aleksejvalenkov@yandex.ru>
2023-11-05 18:38:06 +0300

    add threading fix 7

--- data_transfer/client.py
+++ data_transfer/client.py
@@ -11,20 +11,19 @@ def make_req(sock):
     sock.send("get_im".encode('utf8'))
 
 
-while True:
-    
-    make_req(s)
-    file = open(filename, "wb")
-    while True:
-        file_data = s.recv(4096)
-        file.write(file_data)
-        if not file_data:
-            break
 
-    file.close()
-    print("file downloaded")
-    
-    print(file_data)
-    # cv2.imshow("Camera", im)
-    # cv2.waitKey(1)
+make_req(s)
+file = open(filename, "wb")
+while True:
+    file_data = s.recv(4096)
+    file.write(file_data)
+    if not file_data:
+        break
+
+file.close()
+print("file downloaded")
+
+print(file_data)
+# cv2.imshow("Camera", im)
+# cv2.waitKey(1)
 
