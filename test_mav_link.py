a1bc2d9031a00f3a9c6b0193e17e81fdfec8fd24
aleksejvalenkov <aleksejvalenkov@yandex.ru>
2023-11-06 16:01:24 +0300

    br mav

--- test_mav_link.py
+++ test_mav_link.py
@@ -12,3 +12,5 @@ the_connection = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 
 the_connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
+
+print("hhh")
