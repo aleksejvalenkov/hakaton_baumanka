15bbcd1f7e2192115b24a5de70a6a77e69e10fd2
aleksejvalenkov <aleksejvalenkov@yandex.ru>
2023-11-06 20:13:49 +0300

    add auto pilot w

--- autopilot/main.py
+++ autopilot/main.py
@@ -20,7 +20,7 @@ def mavlink_connect(connection_str: str, baud: int):
     return connection
 
 
-connect = mavlink_connect("/dev/ttyACM0", 115200)  # Подключение
+connect = mavlink_connect("/dev/ttyACM0", 57600)  # Подключение
 
 # test_all.arm(connect, 1)
 arm(connect, 1)
