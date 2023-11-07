7bd29d0fa7db6db11a2e62c559e724937171e927
aleksejvalenkov <aleksejvalenkov@yandex.ru>
2023-11-07 14:26:55 +0300

    fly day  test 13

--- data_transfer/esp_uart.py
+++ data_transfer/esp_uart.py
@@ -14,7 +14,7 @@ class serial_reader:
         '''
         flame, magnet_status, sonic_dist= -1, -1, -1
 
-        data_to_send = '#' + audio_code + ',' + magnet_mode + ',' + led_mode + ';'
+        data_to_send = '#' + str(audio_code) + ',' + str(magnet_mode) + ',' + str(led_mode) + ';'
         self.ser.write(data_to_send.encode())
         received_data = self.ser.readline().decode()
         
